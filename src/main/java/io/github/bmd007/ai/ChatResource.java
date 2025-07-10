179fcc494cb02f13e28b10604e703427f34c9845
Mahdi <bmd579@gmail.com>
2025-07-10 12:52:45 +0200

    second

--- src/main/java/io/github/bmd007/ai/ChatResource.java
+++ src/main/java/io/github/bmd007/ai/ChatResource.java
@@ -1,14 +1,12 @@
 package io.github.bmd007.ai;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 
 @RestController
 public class ChatResource {
