ee0cca2635fda274fdc627baddcc2daef6aaba6a
Mahdi <bmd579@gmail.com>
2025-09-15 10:49:15 +0200

    gemini

--- src/main/java/io/github/bmd007/ai/ChatResource.java
+++ src/main/java/io/github/bmd007/ai/ChatResource.java
@@ -1,15 +1,17 @@
 package io.github.bmd007.ai;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.embedding.EmbeddingResponse;
 import org.springframework.ai.image.ImagePrompt;
-import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.openai.OpenAiImageModel;
 import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,19 +24,41 @@ import java.util.concurrent.CompletableFuture;
 @RestController
 public class ChatResource {
 
-    private final ChatClient chatClient;
+    private static final Logger log = LoggerFactory.getLogger(ChatResource.class);
+
+    private final ChatClient ollamaClient;
     private final EmbeddingModel embeddingModel;
     private final OpenAiImageModel openAiImageModel;
+    private final ChatClient geminiClient;
 
     public ChatResource(OllamaChatModel model,
                         @Qualifier("ollamaEmbeddingModel") EmbeddingModel embeddingModel,
-                        OpenAiImageModel openAiImageModel) {
-        this.chatClient = ChatClient.create(model)
+                        OpenAiImageModel openAiImageModel,
+                        VertexAiGeminiChatModel vertexAiGeminiChatModel) {
+        this.ollamaClient = ChatClient.create(model)
             .mutate()
-            .defaultSystem("You are a friendly chat bot that answers question in the voice of a Pirate.")
+            .defaultSystem("You are a friendly chat bot that. You have access to tools. Use them to answer.")
+            .build();
+        this.geminiClient = ChatClient.create(vertexAiGeminiChatModel)
+            .mutate()
+            .defaultSystem("You are a friendly chat bot that. You have access to tools. Use them to answer.")
             .build();
         this.embeddingModel = embeddingModel;
         this.openAiImageModel = openAiImageModel;
+    }
+
+    @GetMapping("/chat/gemini")
+    public String chatGemini(@RequestParam String input) {
+        var prompt = Prompt.builder()
+            .content(input)
+            .chatOptions(ChatOptions.builder()
+                .temperature(0d)
+                .build())
+            .build();
+        return geminiClient.prompt(prompt)
+            .tools(new TooBox())
+            .call()
+            .content();
     }
 
     @GetMapping("/chat")
@@ -42,31 +66,32 @@ public class ChatResource {
         var prompt = Prompt.builder()
             .content(input)
             .chatOptions(ChatOptions.builder()
-                .temperature(0.1)
+                .temperature(0d)
                 .build())
             .build();
-
-        CompletableFuture.runAsync(() -> {
-                ImageResponse response = openAiImageModel.call(
-                    new ImagePrompt("A light cream colored mini golden Desert eagle",
-                        OpenAiImageOptions.builder()
-                            .quality("hd")
-                            .N(1)
-                            .height(1024)
-                            .width(1024).build())
-                );
-                System.out.println(response.getResult().getOutput().getUrl());
-            })
-            .exceptionally(throwable -> {
-                System.out.println(throwable);
-                return null;
-            })
-            .join();
-
-        return chatClient.prompt(prompt)
-            .tools(new DateTimeTools())
+        return ollamaClient.prompt(prompt)
+            .tools(new TooBox())
             .call()
             .content();
+    }
+
+    @GetMapping("/image")
+    public String imageGenerator(@RequestParam String input) {
+        return CompletableFuture.supplyAsync(() -> openAiImageModel.call(
+                new ImagePrompt(input, OpenAiImageOptions.builder()
+                    .quality("hd")
+                    .N(1)
+                    .height(1024)
+                    .width(1024).build())
+            ))
+            .exceptionally(throwable -> {
+                log.error("Error generating image", throwable);
+                return null;
+            })
+            .join()
+            .getResult()
+            .getOutput()
+            .getUrl();
     }
 
     @GetMapping("/embed")
--- src/main/java/io/github/bmd007/ai/TooBox.java
+++ src/main/java/io/github/bmd007/ai/TooBox.java
@@ -6,17 +6,19 @@ import org.springframework.ai.tool.annotation.Tool;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class DateTimeTools {
+public class TooBox {
     @Tool(description = "Get the current date and time in the user's timezone, provided in ISO-8601 format")
-    String getCurrentDateTime() {
-        var d = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME).toString();
+    public String getCurrentDateTime() {
+        var d = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
         System.out.println("Current time is: " + d);
         return d;
     }
 
     @Tool(description = "Set a user alarm for the given time, provided in ISO-8601 format")
-    void setAlarm(String time) {
-        LocalDateTime alarmTime = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME);
+    public void setAlarm(String time) {
+        var alarmTime = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME);
         System.out.println("Alarm set for " + alarmTime);
     }
+
 }
+
